8c5a6d4e91a866983842090cc5d5d46ed2e65dc5
xxxxyp <wujieye1023@163.com>
2020-06-18 10:21:41 +0800

    打印项目地址访问路径以及swagger文档路径

--- src/main/java/com/fourboy/MoneyCenterApplication.java
+++ src/main/java/com/fourboy/MoneyCenterApplication.java
@@ -1,13 +1,29 @@
 package com.fourboy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
+@Slf4j
 public class MoneyCenterApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MoneyCenterApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(MoneyCenterApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        if (path == null) {
+            path = "";
+        }
+        log.info("\n----------------------------------------------------------\n\tApplication midland-zhuanjie-web is running! Access URLs:\n\tLocal: \t\thttp://localhost:" + port + path + "/\n\tExternal: \thttp://" + ip + ":" + port + path + "/\n\tswagger-ui: \thttp://" + ip + ":" + port + path + "/swagger-ui.html\n\tDoc: \t\thttp://" + ip + ":" + port + path + "/doc.html\n----------------------------------------------------------");
+
     }
 
 }
